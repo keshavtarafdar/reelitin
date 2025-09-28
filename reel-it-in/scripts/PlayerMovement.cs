0cbee9e780530fc39136be259f19ae38b8949ab3
eBoeh <boehlinge26@gmail.com>
2025-09-28 14:49:37 -0400

    Added rowing and idle animations. A quick outline of what I did to implemnt this: My animations are stored in the AnimatedSprite2D node that is a sub node of the "Player" node. These animations are then key framed and timed according in the AnimationPlayer node (a sub node fo player as well). Finally, to have the left and right versions of each animation play according to user inputs I created an animationTree node. In this animation tree there are BlendSpaces that play left or right animations based on user input. The Animation tree node is the controller of when and how animations of the player will play.

--- reel-it-in/scripts/PlayerMovement.cs
+++ reel-it-in/scripts/PlayerMovement.cs
@@ -3,20 +3,57 @@ using System;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-	[Export] public float Speed = 200f;
+	[Export] public float MaxSpeed = 120f;
+	[Export] public float Acceleration = 150f;
+	[Export] public float Friction = 150f;
+
+	private AnimationTree _animTree;
+	private AnimationNodeStateMachinePlayback _animState;
+
+	private float _lastDirection = 1f; // 1 = right, -1 = left
+
+	public override void _Ready()
+	{
+		_animTree = GetNode<AnimationTree>("AnimationTree");
+		_animTree.Active = true;
+
+		_animState = (AnimationNodeStateMachinePlayback)_animTree.Get("parameters/playback");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		float deltaF = (float)delta;
 
-		// Only horizontal movement
-		velocity.X = 0;
-		velocity.Y = 0; // optional if you never move vertically
+		// Input (-1 = left, 1 = right, 0 = none)
+		float inputDir = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+		
+		// Update last facing direction if input exists
+		if (inputDir != 0)
+			_lastDirection = inputDir;
 
-		if (Input.IsActionPressed("ui_left"))
-			velocity.X = -Speed;
-		else if (Input.IsActionPressed("ui_right"))
-			velocity.X = Speed;
+		if (inputDir != 0)
+		{
+			// Move character
+			velocity.X = Mathf.MoveToward(velocity.X, inputDir * MaxSpeed, Acceleration * deltaF);
+
+			// Update Row blend based on last direction
+			_animTree.Set("parameters/Row/BlendSpace1D/blend_position", _lastDirection);
+
+			// Switch to Row animation
+			_animState.Travel("Row");
+		}
+		else
+		{
+			// Apply friction
+			velocity.X = Mathf.MoveToward(velocity.X, 0, Friction * deltaF);
+
+			// Set Idle blend based on last direction
+			_animTree.Set("parameters/Idle/BlendSpace1D/blend_position", _lastDirection);
+
+			// Switch to Idle
+			_animState.Travel("Idle");
+		}
 
 		Velocity = velocity;
 		MoveAndSlide();
