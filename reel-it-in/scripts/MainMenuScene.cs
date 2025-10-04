e92b2db20337f0c76be5c32f6986db677f973c8c
WaddillP <wadplattuva@gmail.com>
2025-10-04 18:35:05 -0400

    Added fade in and fade out transition

--- reel-it-in/scripts/MainMenuScene.cs
+++ reel-it-in/scripts/MainMenuScene.cs
@@ -3,9 +3,11 @@ using System;
 
 public partial class MainMenuScene : Control
 {
+	private AnimationPlayer _animationPlayer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_animationPlayer = GetNode<AnimationPlayer>("ColorRect/AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +17,13 @@ public partial class MainMenuScene : Control
 	
 	private void _on_go_fish_button_pressed()
 	{
+		_animationPlayer.Connect("animation_finished", new Callable(this, nameof(OnAnimationFinished)));
+		_animationPlayer.Play("Fade");
+		
+		
+	}
+	private void OnAnimationFinished(string animName){
+		
 		GetTree().ChangeSceneToFile("res://scenes/RiverScene.tscn");
 	}
 	
