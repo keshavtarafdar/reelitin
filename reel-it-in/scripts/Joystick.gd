515d19ab754ad457355332026d6ff212c0463faa
Keshav <keshavtarafdar@gmail.com>
2025-10-12 12:38:45 -0400

    FIx joystick element, snap to center of control onReady()

--- reel-it-in/scripts/Joystick.gd
+++ reel-it-in/scripts/Joystick.gd
@@ -1,4 +1,53 @@
+class_name Joystick
 extends Control
 
-# Holds the normalized input from the joystick (between -1 and 1)
+@onready var knob: Sprite2D = $Knob
+@onready var ring: Sprite2D = $Ring
 var position_vector: Vector2 = Vector2.ZERO
+var _is_pressing: bool = false
+var center: Vector2
+
+# Calculate center of joystick control based on size, put knob there
+func _ready() -> void:
+	center = size / 2.0
+	knob.position = center
+	ring.position = center
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		_is_pressing = event.is_pressed()
+	if event is InputEventMouseMotion and _is_pressing:
+		knob.position = get_local_mouse_position()
+
+func _process(delta: float) -> void:
+	var knob_max_length = knob.get("max_length")
+	
+	var pos_offset: Vector2 = knob.position
+	var distance: float = (pos_offset - center).length()
+	var current_max_length: float = knob_max_length * scale.x
+	
+	# Normalize inputs which are outside of the joystick's range/ring
+	if distance > current_max_length:
+		knob.position = center + (pos_offset - center).normalized() * current_max_length
+	
+	# Smoothly slide back to center of ring
+	if not _is_pressing:
+		knob.position = knob.position.lerp(center, delta * 10.0)
+
+	_calculate_vector()
+
+
+func _calculate_vector() -> void:
+	var knob_max_length = knob.get("max_length")
+	var knob_dead_zone = knob.get("dead_zone")
+
+	var diff: Vector2 = knob.position - center
+	var current_max_length: float = knob_max_length * scale.x
+
+	var output_vector := Vector2.ZERO
+	if abs(diff.x) >= knob_dead_zone:
+		output_vector.x = diff.x / current_max_length
+	if abs(diff.y) >= knob_dead_zone:
+		output_vector.y = diff.y / current_max_length
+
+	position_vector = output_vector.clamp(Vector2(-1, -1), Vector2(1, 1))
--- reel-it-in/scripts/PlayerMovement.gd
+++ reel-it-in/scripts/PlayerMovement.gd
@@ -3,31 +3,26 @@ extends CharacterBody2D
 @export var max_speed: float = 120.0
 @export var acceleration: float = 100.0
 @export var friction: float = 100.0
-@export var player_joystick: Node
+@export var player_joystick: Joystick
 
-var _anim_tree: AnimationTree
-var _anim_state
+@onready var _anim_tree: AnimationTree = $AnimationTree
+@onready var _anim_state = _anim_tree["parameters/playback"]
 var _last_direction: float = 1.0 # 1 = right, -1 = left
 
 func _ready() -> void:
-	_anim_tree = $AnimationTree
 	_anim_tree.active = true
-	_anim_state = _anim_tree.get("parameters/playback")
 
 func _physics_process(delta: float) -> void:
-	var vel = self.velocity
-
-	var input_dir = player_joystick.position_vector.x if player_joystick else 0.0
+	var input_dir: float = player_joystick.position_vector.x
 
 	if input_dir != 0:
 		_last_direction = sign(input_dir)
-		vel.x = move_toward(vel.x, input_dir * max_speed, acceleration * delta)
+		velocity.x = move_toward(velocity.x, input_dir * max_speed, acceleration * delta)
 		_anim_tree.set("parameters/Row/BlendSpace1D/blend_position", _last_direction)
 		_anim_state.travel("Row")
 	else:
-		vel.x = move_toward(vel.x, 0, friction * delta)
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
 		_anim_tree.set("parameters/Idle/BlendSpace1D/blend_position", _last_direction)
 		_anim_state.travel("Idle")
 
-	self.velocity = vel
 	move_and_slide()
