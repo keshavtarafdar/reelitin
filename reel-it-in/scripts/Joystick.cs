78573022fb888b46d671e7744ed67edb58cc066c
eBoeh <boehlinge26@gmail.com>
2025-10-12 11:35:21 -0400

    Chab=nged everything to GDScript

--- reel-it-in/scripts/Joystick.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-
-public partial class Joystick : Control
-{
-	// Hold the normalized input from joystick (between -1 and 1)
-	public Vector2 PositionVector {get; set; } = Vector2.Zero;
-}
---
+++ reel-it-in/scripts/Joystick.gd
@@ -0,0 +1,4 @@
+extends Control
+
+# Holds the normalized input from the joystick (between -1 and 1)
+var position_vector: Vector2 = Vector2.ZERO
