4fb8af8e8e75a115fe91e43ae12a63c47de213b8
eBoeh <boehlinge26@gmail.com>
2025-10-25 16:21:53 -0400

    Building out the hook state machine. Made it so only one fish can be interested in the hook at a time.

--- reel-it-in/scenes/hook.gd
+++ reel-it-in/scenes/hook.gd
@@ -2,31 +2,43 @@ extends CharacterBody2D
 
 @export var player : CharacterBody2D
 @export var fish : CharacterBody2D # Dynamically assigned
+
+
 # Hook physics variables
 var water_friction: int = 10
 
-
-
 # State tracking
 enum mobState {
-	HOOKED,
+	INVISIBLE,
 	CASTED,
 	FLOATING,
-	INVISIBLE
+	HOOKED,
+	REELING
 }
 var current_state: int
 
 func _ready():
 	current_state = mobState["FLOATING"] # Set to floating for testing
 
+func checkForFish() -> void:
+	for child in self.get_children():
+		if child.is_in_group("Fish"):
+			fish = child
+			current_state = mobState['HOOKED']
 
 func _physics_process(delta: float) -> void:
 	
 	match current_state:
 		
 		mobState["HOOKED"]:
-			pass
+			checkForFish()
+			var fish_velocity = fish.swim_physic(delta)
+			
 		mobState["CASTED"]:
 			pass
 		mobState["FLOATING"]:
 			self.velocity = velocity.move_toward(Vector2.ZERO, water_friction * delta)
+		mobState["REELING"]:
+			pass
+	
+	move_and_slide()
