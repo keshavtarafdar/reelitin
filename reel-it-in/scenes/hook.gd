c88ff52d0fbbdc64871870791dbf99d66a62e924
eBoeh <boehlinge26@gmail.com>
2025-10-25 13:59:54 -0400

    Refined some aspects of the fish.gd script. Added hook.gd. This script will have a mix of a state machine and user inputs to control the hook movement.

--- reel-it-in/scenes/hook.gd
+++ reel-it-in/scenes/hook.gd
@@ -1,4 +1,32 @@
 extends CharacterBody2D
 
+@export var player : CharacterBody2D
+@export var fish : CharacterBody2D # Dynamically assigned
+# Hook physics variables
+var water_friction: int = 10
+
+
+
+# State tracking
+enum mobState {
+	HOOKED,
+	CASTED,
+	FLOATING,
+	INVISIBLE
+}
+var current_state: int
+
+func _ready():
+	current_state = mobState["FLOATING"] # Set to floating for testing
+
+
 func _physics_process(delta: float) -> void:
-	pass
+	
+	match current_state:
+		
+		mobState["HOOKED"]:
+			pass
+		mobState["CASTED"]:
+			pass
+		mobState["FLOATING"]:
+			self.velocity = velocity.move_toward(Vector2.ZERO, water_friction * delta)
