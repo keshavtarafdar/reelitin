a261a77614f625df73e302520cba89a5b24075f5
eBoeh <boehlinge26@gmail.com>
2025-10-27 16:29:19 -0400

    Set up steps

--- GodotPlugin/Package.swift
+++ GodotPlugin/Package.swift
@@ -1,23 +1,35 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GodotPlugin",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Defines the dynamic library that will be built for Godot
         .library(
             name: "GodotPlugin",
+            type: .dynamic,
             targets: ["GodotPlugin"]
         ),
     ],
+    dependencies: [
+        // Main SwiftGodot dependency
+        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GodotPlugin"
+            name: "GodotPlugin",
+            dependencies: [
+                .product(name: "SwiftGodot", package: "SwiftGodot")
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-suppress-warnings"])
+            ]
         ),
-
     ]
 )
--- GodotPlugin/Sources/GodotPlugin/GodotPlugin.swift
+++ GodotPlugin/Sources/GodotPlugin/GodotPlugin.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftGodot
+
+#initSwiftExtension(
+    cdecl: "swift_entry_point",
+    types: [GodotPlugin.self]
+)
+
+@Godot
+class GodotPlugin: RefCounted {
+   
+    // Define a signal with one argument (String)
+    nonisolated(unsafe) static let output = SignalWith1Argument<String>("Output")
+
+    @Callable
+    func connectToGodot() -> String {
+        return "You are now focusing!"
+    }
+}
