4b8814e98464fa73512feecbabf62f6ed652573e
eBoeh <boehlinge26@gmail.com>
2025-10-28 20:50:06 -0400

    Moved Godot Plugin back out lol.

--- reel-it-in/scripts/MainMenuScene.gd
+++ reel-it-in/scripts/MainMenuScene.gd
@@ -20,5 +20,5 @@ func _on_go_fish_button_pressed() -> void:
 func _on_animation_finished(_anim_name: String) -> void:
 	get_tree().change_scene_to_file("res://scenes/RiverScene.tscn")
 
-func _on_focus_mode_button_pressed(data) -> void:
-	print("test")
+func _on_focus_mode_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/FocusScene.tscn")
