ff5ba69f0ab946a7c26b7dac01f5aceb0b857f1d
Keshav <keshavtarafdar@gmail.com>
2025-11-03 14:46:30 -0500

    Add framework imports

--- GodotPlugin/Sources/GodotPlugin/GodotPlugin.swift
+++ GodotPlugin/Sources/GodotPlugin/GodotPlugin.swift
@@ -1,6 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import SwiftGodot
+import FamilyControls
+import ManagedSettings
+import UIKit
 
 #initSwiftExtension(
     cdecl: "swift_entry_point",
