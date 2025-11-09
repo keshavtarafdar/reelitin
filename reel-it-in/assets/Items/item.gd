63f879714fa8087d31679b6d3bceb44e1ca730a6
eBoeh <boehlinge26@gmail.com>
2025-11-09 18:56:20 -0500

    Inventory setup + adding one item sceen as the fish. Reorganized scripts.

---
+++ reel-it-in/assets/Items/item.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Item
+
+@export var name: String
+@export_enum("Fish", "Equipment", "Bait") var type: String
+@export var inv_icon: Texture2D
+@export var item_path: String
