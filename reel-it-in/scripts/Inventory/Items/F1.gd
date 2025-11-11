c6f448d551a8f1150e03971af63e6ecb4856beb2
eBoeh <boehlinge26@gmail.com>
2025-11-10 22:15:43 -0500

    Finished inventory + catch system (lots to explain, please call me if you want to run though how it works).

--- reel-it-in/scripts/Inventory/Items/F1.gd
+++ reel-it-in/scripts/Inventory/Items/F1.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 @export var item_res : Item
-@export var fish_CPU : Fish
-
 @onready var inv = $"../Player/Inventory"
 
 
@@ -10,7 +8,5 @@ func _on_timer_timeout() -> void:
 	$Button.disabled = false
 
 func _on_button_pressed() -> void:
-	print(1)
 	if inv.add_item(inv.prep_item(self)):
-		fish_CPU.queue_free()
 		self.queue_free()
