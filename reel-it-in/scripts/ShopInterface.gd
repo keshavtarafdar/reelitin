c71949ebf29643f77a43d53f5307927e1a743827
eBoeh <boehlinge26@gmail.com>
2025-11-16 12:46:22 -0500

    Refined

--- reel-it-in/scripts/ShopInterface.gd
+++ reel-it-in/scripts/ShopInterface.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
 @onready var joystick = $"../UI/Joystick"
+@onready var bag_button = $"../Camera2D/UIScale/InventoryButton"
+@onready var inventory = $"../Inventory"
+
+var inv_offset = Vector2(-130, -57)
 
 
 func _on_exit_button_pressed() -> void:
 	self.visible = false
 	joystick.visible = true
+	bag_button.visible = true
+	inventory.visible = false
+	inventory.position -= inv_offset
