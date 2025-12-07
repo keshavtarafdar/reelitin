0ade3cd2cdba65ca45342b3cf8e02bff6fb66897
eBoeh <boehlinge26@gmail.com>
2025-12-06 22:33:20 -0500

    Final music changes

--- reel-it-in/scripts/SFX.gd
+++ reel-it-in/scripts/SFX.gd
@@ -11,10 +11,10 @@ var money = preload("res://assets/sounds/money.mp3")
 var shop = preload("res://assets/sounds/shop.mp3")
 var music = preload("res://assets/sounds/Patience.wav")
 
-# Keep track of currently playing AudioStreamPlayers
 var active_players := []
 var music_player: AudioStreamPlayer = null
 var music_fade_task_running := false
+
 
 func play(sound, volume_db := 0, random_pitch := false, single_instance := false, looped := false):
 	if single_instance:
@@ -23,18 +23,11 @@ func play(sound, volume_db := 0, random_pitch := false, single_instance := false
 				return
 
 	var p = AudioStreamPlayer.new()
-
-	if looped:
-		var clone = sound.duplicate()
-		clone.loop = true
-		p.stream = clone
-	else:
-		p.stream = sound
-
+	p.stream = sound
 	p.volume_db = volume_db
 	
 	if random_pitch:
-		p.pitch_scale = 1.0 + randf_range(-0.05, 0.05)  # ±5% pitch variation
+		p.pitch_scale = 1.0 + randf_range(-0.05, 0.05)
 
 	add_child(p)
 	p.play()
@@ -50,7 +43,7 @@ func _on_player_finished(player):
 		active_players.erase(player)
 	player.queue_free()
 
-# Stop a specific sound type
+
 func stop_sound(sound):
 	var to_remove := []
 	for player in active_players:
@@ -62,19 +55,38 @@ func stop_sound(sound):
 		active_players.erase(player)
 
 
-func _fade_volume(player: AudioStreamPlayer, from_db: float, to_db: float, duration: float) -> void:
-	var tween := create_tween()
-	tween.tween_property(player, "volume_db", to_db, duration).from(from_db)
-	await tween.finished
+# Perceptually linear fade
+func _fade_volume_perceptual(player: AudioStreamPlayer, duration: float, target_db: float, start_db: float = -60.0) -> void:
+	var time := 0.0
+	var start_gain := pow(10.0, start_db / 20.0)
+	var end_gain := pow(10.0, target_db / 20.0)
+
+	while time < duration:
+		var t := time / duration
+		# Apply quadratic easing to slow down the start
+		var eased_t := t * t
+		var gain = lerp(start_gain, end_gain, eased_t)
+		player.volume_db = 20.0 * log(max(gain, 0.0001)) / log(10)
+		await get_tree().process_frame
+		time += get_process_delta_time()
+
+	player.volume_db = target_db
 
 
 
-func play_random_fading_music(min_silence := 10.0, max_silence := 20.0, 
-							  fade_in_time := 3.0, fade_out_time := 3.0,
-							  min_play_time := 20.0, max_play_time := 40.0, volume := -10):
+
+func _on_music_finished():
+	if music_player:
+		music_player.play()
+
+
+func play_random_fading_music(min_silence := 1, max_silence := 2, 
+							  fade_in_time := 30.0, fade_out_time := 30.0,
+							  min_play_time := 60.0, max_play_time := 120.0,
+							  volume := -21):
 
 	if music_fade_task_running:
-		return  # prevent duplicate loops
+		return
 
 	music_fade_task_running = true
 
@@ -83,17 +95,20 @@ func play_random_fading_music(min_silence := 10.0, max_silence := 20.0,
 
 		music_player = AudioStreamPlayer.new()
 		music_player.stream = music
-		music_player.loop = true
-		music_player.volume_db = volume
+		music_player.volume_db = -60   # start very silent
 		add_child(music_player)
 
-		music_player.play()
+		music_player.connect("finished", Callable(self, "_on_music_finished"))
 
-		await _fade_volume(music_player, volume, 0, fade_in_time)
+		var stream_length := music.get_length()
+		var start_pos := randf_range(0.0, stream_length)
+		music_player.play(start_pos)
+
+		await _fade_volume_perceptual(music_player, fade_in_time, volume, -60)
 
 		await get_tree().create_timer(randf_range(min_play_time, max_play_time)).timeout
 
-		await _fade_volume(music_player, 0, volume, fade_out_time)
+		await _fade_volume_perceptual(music_player, fade_out_time, -60, volume)
 
 		music_player.stop()
 		music_player.queue_free()
